0aedf92ea532bb0b93103ccb7ab88c969ffb8ee4
Tsuyoshi Ushio <ushio@simplearchitect.com>
2019-11-02 17:36:35 -0700

    The first implementation of the gRPC server

---
+++ src/Keda.Durable.Scaler.Server/Services/ExternalScalerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Keda.Durable.Scaler.Server.Protos;
+
+namespace Keda.Durable.Scaler.Server.Services
+{
+    public class ExternalScalerService : ExternalScaler.ExternalScalerBase
+    {
+        public override Task<Empty> New(NewRequest request, ServerCallContext context)
+        {
+            return base.New(request, context);
+        }
+
+        public override Task<IsActiveResponse> IsActive(ScaledObjectRef request, ServerCallContext context)
+        {
+            return base.IsActive(request, context);
+        }
+
+        public override Task<GetMetricSpecResponse> GetMetricSpec(ScaledObjectRef request, ServerCallContext context)
+        {
+            return base.GetMetricSpec(request, context);
+        }
+
+        public override Task<GetMetricsResponse> GetMetrics(GetMetricsRequest request, ServerCallContext context)
+        {
+            return base.GetMetrics(request, context);
+        }
+
+        public override Task<Empty> Close(ScaledObjectRef request, ServerCallContext context)
+        {
+            return base.Close(request, context);
+        }
+    }
+}
