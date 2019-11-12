a93e49bd6a17fcbe1150caa520cc72ea501e4b98
Tsuyoshi Ushio <ushio@simplearchitect.com>
2019-11-11 22:05:51 -0800

    Enable simple integration testing

--- tests/Keda.Durable.Scaler.Integration.Test/ScaleScenarioTests.cs
+++ tests/Keda.Durable.Scaler.Integration.Test/ScaleScenarioTests.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Threading.Tasks;
 using DurableTask.AzureStorage.Monitoring;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Keda.Durable.Scaler.Integration.Protos;
 using Keda.Durable.Scaler.Server.Protos;
 using Keda.Durable.Scaler.Server.Repository;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Xunit;
 using ExternalScaler = Keda.Durable.Scaler.Integration.Protos.ExternalScaler;
 using GetMetricsRequest = Keda.Durable.Scaler.Integration.Protos.GetMetricsRequest;
+using NewRequest = Keda.Durable.Scaler.Integration.Protos.NewRequest;
+using ScaledObjectRef = Keda.Durable.Scaler.Integration.Protos.ScaledObjectRef;
 
 namespace Keda.Durable.Scaler.Integration.Test
 {
@@ -23,10 +27,16 @@ namespace Keda.Durable.Scaler.Integration.Test
                 host.Start();
                 var channel = GrpcChannel.ForAddress("http://localhost:5000");
                 var client = new ExternalScaler.ExternalScalerClient(channel);
-                var request = new GetMetricsRequest();
+                var request = new NewRequest();
+                var scaleObjectRef = new ScaledObjectRef();
+                scaleObjectRef.Namespace = "foo";
+                scaleObjectRef.Name = "bar";
 
-                var reply = client.GetMetrics(request);
-                Assert.Equal("hello", reply.MetricValues[0].MetricName);
+                var reply = client.New(request, new CallOptions());
+                Assert.NotNull(reply);  // Make sure gRPC call works. 
+
+                var isActive = client.IsActive(scaleObjectRef, new CallOptions());
+                Assert.True(isActive.Result);
             }
         }
 
@@ -37,7 +47,9 @@ namespace Keda.Durable.Scaler.Integration.Test
             public override void ConfigureServices(IServiceCollection services) 
             {
                 base.ConfigureServices(services);
+                services.AddGrpc();
                 services.AddSingleton<IPerformanceMonitorRepository>(new MockSimplePerformanceMonitorRepository());
+                services.AddSingleton<IKubernetesRepository>(new MockKubernetesRepository());
             }
         }
         private class MockSimplePerformanceMonitorRepository : IPerformanceMonitorRepository
@@ -45,6 +57,14 @@ namespace Keda.Durable.Scaler.Integration.Test
             public Task<PerformanceHeartbeat> PulseAsync(int currentWorkerCount)
             {
                 return Task.FromResult(new PerformanceHeartbeat());
+            }
+        }
+
+        private class MockKubernetesRepository : IKubernetesRepository
+        {
+            public Task<int> GetNumberOfPodAsync(string deploymentName, string nameSpace)
+            {
+                return Task.FromResult(1);
             }
         }
     }
